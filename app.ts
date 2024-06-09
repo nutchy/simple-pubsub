9afebaad0785e4fbeb1c22aa648685b40c7edf3c
Nutchy <work@nutchy.dev>
2024-06-09 17:13:35 +0700

    test: initial test

--- app.ts
+++ app.ts
@@ -9,31 +9,39 @@ interface ISubscriber {
 }
 
 interface IPublishSubscribeService {
-  publish (event: IEvent): void;
-  subscribe (type: string, handler: ISubscriber): void;
+  publish(event: IEvent): void;
+  subscribe(type: string, handler: ISubscriber): void;
   // unsubscribe ( /* Question 2 - build this feature */ );
 }
 
+// ==================================================================================================
 
-// implementations
+// Event implementations
 class MachineSaleEvent implements IEvent {
-  constructor(private readonly _sold: number, private readonly _machineId: string) {}
+  constructor(
+    private readonly _sold: number,
+    private readonly _machineId: string
+  ) {}
 
   machineId(): string {
     return this._machineId;
   }
 
   getSoldQuantity(): number {
-    return this._sold
+    return this._sold;
   }
 
   type(): string {
-    return 'sale';
+    return "sale"; // TODO: change to constant or Event Type
   }
 }
 
+// Todo: (3) Implement MachineRefillSubscriber. It will increase the stock quantity of the machine.
 class MachineRefillEvent implements IEvent {
-  constructor(private readonly _refill: number, private readonly _machineId: string) {}
+  constructor(
+    private readonly _refill: number,
+    private readonly _machineId: string
+  ) {}
 
   machineId(): string {
     throw new Error("Method not implemented.");
@@ -44,11 +52,14 @@ class MachineRefillEvent implements IEvent {
   }
 }
 
+// ==================================================================================================
+
+// Subscriber implementations
 class MachineSaleSubscriber implements ISubscriber {
   public machines: Machine[];
 
-  constructor (machines: Machine[]) {
-    this.machines = machines; 
+  constructor(machines: Machine[]) {
+    this.machines = machines;
   }
 
   handle(event: MachineSaleEvent): void {
@@ -62,54 +73,67 @@ class MachineRefillSubscriber implements ISubscriber {
   }
 }
 
+// ==================================================================================================
 
-// objects
+class MachineEventPublisher implements IPublishSubscribeService {
+  // TODO: add constructor to register event subsciber
+
+  publish(event: IEvent): void {}
+
+  subscribe(type: string, handler: ISubscriber): void {}
+}
+
+// objects (model)
 class Machine {
   public stockLevel = 10;
   public id: string;
 
-  constructor (id: string) {
+  constructor(id: string) {
     this.id = id;
   }
 }
-
 
 // helpers
 const randomMachine = (): string => {
   const random = Math.random() * 3;
   if (random < 1) {
-    return '001';
+    return "001";
   } else if (random < 2) {
-    return '002';
+    return "002";
   }
-  return '003';
-
-}
+  return "003";
+};
 
 const eventGenerator = (): IEvent => {
   const random = Math.random();
   if (random < 0.5) {
     const saleQty = Math.random() < 0.5 ? 1 : 2; // 1 or 2
     return new MachineSaleEvent(saleQty, randomMachine());
-  } 
+  }
   const refillQty = Math.random() < 0.5 ? 3 : 5; // 3 or 5
   return new MachineRefillEvent(refillQty, randomMachine());
-}
-
+};
 
 // program
 (async () => {
   // create 3 machines with a quantity of 10 stock
-  const machines: Machine[] = [ new Machine('001'), new Machine('002'), new Machine('003') ];
+  const machines: Machine[] = [
+    new Machine("001"),
+    new Machine("002"),
+    new Machine("003"),
+  ];
 
   // create a machine sale event subscriber. inject the machines (all subscribers should do this)
   const saleSubscriber = new MachineSaleSubscriber(machines);
 
   // create the PubSub service
-  const pubSubService: IPublishSubscribeService = null as unknown as IPublishSubscribeService; // implement and fix this
+  // const pubSubService: IPublishSubscribeService =
+  //   null as unknown as IPublishSubscribeService; // implement and fix this
+
+  const pubSubService: IPublishSubscribeService = new MachineEventPublisher();
 
   // create 5 random events
-  const events = [1,2,3,4,5].map(i => eventGenerator());
+  const events = [1, 2, 3, 4, 5].map((i) => eventGenerator());
 
   // publish the events
   events.map(pubSubService.publish);
---
+++ events/MachineSaleEvent.test.ts
@@ -0,0 +1,7 @@
+import { expect, test } from "@jest/globals";
+
+describe("Initial unit test", () => {
+  test("test", () => {
+    expect(1 + 1).toEqual(2);
+  });
+});
