93f757568e425dc8259a016957821501b4eed03c
Nutchy <work@nutchy.dev>
2024-06-10 04:03:46 +0700

    fix: construct subscriber in app.js

--- app.ts
+++ app.ts
@@ -1,27 +1,39 @@
+import { StockLevelOkSubscriber } from './subscribers/stockLevelOkSubscriber';
+import { LowStockWarningSubscriber } from './subscribers/lowStockWarningSubscriber';
 import { IPublishSubscribeService } from "./events";
-import { MachineEventPublisher } from "./publishers/MachineEventPublisher";
-import { MachineSaleSubscriber } from "./subscribers/MachineSaleSubscriber";
+import { MachineEventPublisher } from "./publishers/machineEventPublisher";
+import { MachineSaleSubscriber } from "./subscribers/machineSaleSubscriber";
 import { eventGenerator } from "./helpers/generator";
 import { Machine } from "./models";
+import { MachineRepository } from "./repository";
+import { MachineRefillSubscriber } from "./subscribers/machineRefillSubscriber";
+import { EventType } from './events/type';
 
 // program
 (async () => {
-  // create 3 machines with a quantity of 10 stock
-  const machines: Machine[] = [
-    new Machine("001"),
-    new Machine("002"),
-    new Machine("003"),
-  ];
+  // 1. create 3 machines with a quantity of 10 stock
+  const machineRepository = MachineRepository.instance
 
-  // create a machine sale event subscriber. inject the machines (all subscribers should do this)
-  const saleSubscriber = new MachineSaleSubscriber(machines);
+  // 2. create the PubSub service with empty subscriber
+  const pubSubService: IPublishSubscribeService = MachineEventPublisher.instance;
 
-  // create the PubSub service
-  const pubSubService: IPublishSubscribeService = new MachineEventPublisher({});
+  // 3. create a machine sale event subscriber. inject the machines (all subscribers should do this)
+  const saleSubscriber = new MachineSaleSubscriber(machineRepository, pubSubService);
+  const refillSubscriber = new MachineRefillSubscriber(machineRepository, pubSubService)
+  const lowStockSubscriber = new LowStockWarningSubscriber()
+  const stockLevelOkSubscriber = new StockLevelOkSubscriber()
 
-  // create 5 random events
+  // 4. register all subscriber into pubsub
+  pubSubService.subscribe(EventType.MachineSale, saleSubscriber)
+  pubSubService.subscribe(EventType.MachineRefill, refillSubscriber)
+  pubSubService.subscribe(EventType.LowStockWarning, lowStockSubscriber)
+  pubSubService.subscribe(EventType.StockLevelOk, stockLevelOkSubscriber)
+
+  // 5. create 5 random events
   const events = [1, 2, 3, 4, 5].map((_) => eventGenerator());
 
-  // publish the events
-  events.map(pubSubService.publish);
+  // 6. publish the events
+  for (const e of events) {
+    pubSubService.publish(e)
+  }
 })();
