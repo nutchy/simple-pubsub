dae59b24ec5d2c8705db4dcb286ca2b326848556
Nutchy <work@nutchy.dev>
2024-06-09 18:25:30 +0700

    test: publish event with specific event type

--- tests/publisher/MachineEventPublisher.test.ts
+++ tests/publisher/MachineEventPublisher.test.ts
@@ -1,3 +1,4 @@
+import { IEvent, ISubscriber } from "../../events";
 import { MachineEventPublisher } from "../../publisher/MachineEventPublisher";
 
 describe("MachineEventPublisher", () => {
@@ -6,10 +7,32 @@ describe("MachineEventPublisher", () => {
 
     expect(publisher.subscribers).toEqual({});
     expect(publisher.subscribers["foo"]).toBeUndefined();
-    
+
     const foo = { handle: jest.fn() };
     publisher.subscribe("foo", foo);
     expect(publisher.subscribers["foo"]).toBeDefined();
     expect(publisher.subscribers["foo"].size).toEqual(1);
+  });
+
+  it("publish event with specific event type", () => {
+    // Mock subsribers
+    const foo = { handle: jest.fn() };
+    const bar = { handle: jest.fn() };
+
+    // Create concrete object with subscribable object
+    const publisher = new MachineEventPublisher({
+      foo: new Set<ISubscriber>().add(foo),
+      bar: new Set<ISubscriber>().add(bar),
+    });
+
+    // Publish foo event
+    const event: IEvent = {
+        machineId: () => "001",
+        type: () => "foo"
+    }
+    publisher.publish(event)
+
+    expect(foo.handle).toHaveBeenCalledTimes(1)
+    expect(bar.handle).toHaveBeenCalledTimes(0)
   });
 });
